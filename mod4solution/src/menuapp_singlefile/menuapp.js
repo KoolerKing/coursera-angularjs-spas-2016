52d25da90557c9bbe638ee3beca3033e19d22025
Kevin Offet <kevinoffet@gmail.com>
2016-10-13 02:58:57 -0400

    simple routing works

--- mod4solution/src/menuapp_singlefile/menuapp.js
+++ mod4solution/src/menuapp_singlefile/menuapp.js
@@ -1,9 +1,27 @@
 (function() {
   'use strict';
-  angular.module('MenuApp',[])
+  angular.module('MenuApp',["ui.router"])
   .service('MenuDataService', MenuDataService)
-  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+  .config(RoutesConfig);
 
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      template: '<div>Home</div>'
+    })
+    .state('categories', {
+      url: '/categories',
+      template: '<div>Categories</div>'
+    })
+    .state('items', {
+      url: '/items',
+      template: '<div>Items</div>'
+    })
+  };
 
   MenuDataService.$inject = ['$http', 'ApiBasePath'];
   function MenuDataService($http, ApiBasePath) {
@@ -14,12 +32,17 @@
         method: 'GET',
         url: ApiBasePath + "/categories.json"
       }).catch(function(error){
-        console.log("Something went very wrong with the network request: ", error);
+        console.log("Something went very wrong with the network request for categories: ", error);
       });
     };
 
     mds.getItemsForCategory = function(categoryShortName) {
-
+      return $http({
+        method: 'GET',
+        url: ApiBasePath + "/menu_items.json?category=" + categoryShortName
+      }).catch(function(error){
+        console.log("Something went very wrong with the network request for menu_items: ", error);
+      });
     };
   };
 
