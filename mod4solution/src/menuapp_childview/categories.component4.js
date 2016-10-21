bac7e84ede34336ff1965ca80468e1d2048ab11b
Kevin Offet <kevinoffet@gmail.com>
2016-10-20 23:19:28 -0400

    fully functioning child view version.

---
+++ mod4solution/src/menuapp_childview/categories.component4.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('MenuApp')
+  .component('menuCategories', {
+    templateUrl: 'src/menuapp_childview/categories.component4.html',
+    bindings: {
+      categories: '<'
+    }
+  });
+
+})();
---
+++ mod4solution/src/menuapp_childview/routes4.js
@@ -0,0 +1,54 @@
+(function() {
+  'use strict';
+
+  angular.module('MenuApp')
+  .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'src/menuapp/homeView.html'
+    })
+    .state('categories', {
+      url: '/categories',
+      templateUrl: 'src/menuapp_childview/categoriesView4.html',
+      controller: 'categoriesViewCtrl as cvc',
+      resolve: {
+        categoriesData: ['MenuDataService', function(MenuDataService) {
+          return MenuDataService.getAllCategories().then(function(response) {
+            //console.log("found categories data: ", response.data);
+            return response.data;
+          });
+        }]
+      },
+      bindings: {
+        categories: '<'
+      }
+    })
+    .state('categories.items', {
+      url: '/items/{itemSN}',
+      templateUrl: 'src/menuapp_childview/itemsView4.html',
+      controller: 'itemsViewCtrl as ivc',
+      resolve: {
+        itemsData: ['MenuDataService', '$stateParams', function(MenuDataService, $stateParams) {
+          var isn = $stateParams.itemSN;
+          //console.log("found parameter: ", isn);
+          return MenuDataService.getItemsForCategory(isn).then(function(response) {
+            //var t2 = response.data.menu_items;
+            //console.log("found response data: ", response.data);
+            //return t2;
+            return response.data;
+          });
+        }]
+      },
+      bindings: {
+        items: '<'
+      }
+    })
+  };
+
+})();
